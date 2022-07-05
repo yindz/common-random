64a6d2389f25a67f1414a9e2236755db634e93d9
yin <yinzhili001@gmail.com>
2022-07-05 20:37:22 +0800

    支持生成随机虚拟借记卡号码

--- src/main/java/com/apifan/common/random/source/FinancialSource.java
+++ src/main/java/com/apifan/common/random/source/FinancialSource.java
@@ -1,10 +1,13 @@
 package com.apifan.common.random.source;
 
+import com.apifan.common.random.constant.CreditCardType;
 import com.apifan.common.random.entity.CurrencyInfo;
 import com.apifan.common.random.entity.KChartData;
 import com.apifan.common.random.util.ResourceUtils;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -54,6 +57,8 @@ public class FinancialSource {
      * 货币信息
      */
     private static List<String> currencyList = Lists.newArrayList();
+
+    private static final String UNION_PAY_PREFIX = "62";
 
     private static final FinancialSource instance = new FinancialSource();
 
@@ -210,5 +215,87 @@ public class FinancialSource {
         currencyInfo.setName(tmp[1]);
         currencyInfo.setCode(tmp[2]);
         return currencyInfo;
+    }
+
+    /**
+     * 随机虚拟信用卡号码
+     *
+     * <p> 特别说明: 不会与现实中的真实信用卡号产生重合 </p>
+     *
+     * @param type 信用卡类型
+     * @return 随机虚拟信用卡号码
+     */
+    public String randomCreditCardNo(CreditCardType type) {
+        String prefix;
+        int length = 12;
+        if (CreditCardType.Visa.equals(type)) {
+            //VISA信用卡前缀为4，增加99是为了避开现实中的信用卡号码
+            prefix = "499";
+        } else if (CreditCardType.MasterCard.equals(type)) {
+            //MasterCard信用卡前缀为5，增加9是为了避开现实中的信用卡号码
+            prefix = "59";
+            length = 13;
+        } else if (CreditCardType.Amex.equals(type)) {
+            //AMEX信用卡前缀为37，增加9是为了避开现实中的信用卡号码
+            prefix = "379";
+        } else if (CreditCardType.UnionPay.equals(type)) {
+            //银联信用卡前缀为62，增加9是为了避开现实中的信用卡号码
+            prefix = UNION_PAY_PREFIX + "9";
+        } else if (CreditCardType.JCB.equals(type)) {
+            //JCB信用卡前缀为35，增加9是为了避开现实中的信用卡号码
+            prefix = "359";
+        } else {
+            prefix = UNION_PAY_PREFIX + "9";
+        }
+        String digits = prefix + RandomStringUtils.randomNumeric(length);
+        return digits + getCheckDigit(digits);
+    }
+
+    /**
+     * 随机虚拟借记卡号码(银联)
+     *
+     * <p> 特别说明: 不会与现实中的真实借记卡号产生重合 </p>
+     *
+     * @return 随机虚拟借记卡号码
+     */
+    public String randomDebitCardNo() {
+        //发卡机构标识码9000~9999为目前尚未实际使用的号段(为了避免不必要的麻烦)
+        String digits = UNION_PAY_PREFIX + RandomUtils.nextInt(9000, 10000) + RandomStringUtils.randomNumeric(12);
+        return digits + getCheckDigit(digits);
+    }
+
+    /**
+     * 简单计算校验码(符合Luhn算法)
+     *
+     * @param toCheck 待计算的数字字符串
+     * @return 校验码
+     */
+    private static String getCheckDigit(String toCheck) {
+        Preconditions.checkArgument(StringUtils.isNotBlank(toCheck), "待计算的数字字符串为空");
+        //所有待计算的数字
+        List<Integer> digits = Lists.newArrayList();
+        int length = toCheck.length();
+        int maxIndex = length - 1;
+        for (int i = maxIndex; i >= 0; i--) {
+            int j = maxIndex - i;
+            int number = Integer.parseInt(String.valueOf(toCheck.charAt(i)));
+            if (j % 2 == 0) {
+                int f = number * 2;
+                if (f >= 10) {
+                    //乘积f大于10时，将十位数与个位数相加重新得到f
+                    f = (f / 10) + (f % 10);
+                }
+                digits.add(f);
+            } else {
+                digits.add(number);
+            }
+        }
+        //求和
+        int total = digits.stream().mapToInt(Integer::intValue).sum();
+        //总和除以10取余数
+        int mod = total % 10;
+        //余数=0时校验码为0，余数大于0时校验码为10-余数
+        int digit = (mod == 0) ? 0 : 10 - mod;
+        return String.valueOf(digit);
     }
 }
